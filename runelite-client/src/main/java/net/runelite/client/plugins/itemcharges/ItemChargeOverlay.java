1835ace865b584c122dceecd568a35ed2b0b6972
l2- <xanderhermans@hotmail.com>
2019-06-06 15:29:58 +0200

    position fix itemcharges

--- runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargeOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/itemcharges/ItemChargeOverlay.java
@@ -123,8 +123,7 @@ class ItemChargeOverlay extends WidgetItemOverlay
 
 		final Rectangle bounds = itemWidget.getCanvasBounds();
 		final TextComponent textComponent = new TextComponent();
-//		textComponent.setPosition(new Point(bounds.x, bounds.y + 1 + graphics.getFontMetrics().getMaxAscent() - graphics.getFontMetrics().getMaxDescent()));
-		textComponent.setPosition(new Point(bounds.x - 1, bounds.y + graphics.getFontMetrics().getMaxAscent() - graphics.getFontMetrics().getMaxDescent()));
+		textComponent.setPosition(new Point(bounds.x, bounds.y + graphics.getFontMetrics().getMaxAscent() - graphics.getFontMetrics().getMaxDescent()));
 		textComponent.setText(charges < 0 ? "?" : String.valueOf(charges));
 		textComponent.setColor(itemChargePlugin.getColor(charges));
 		textComponent.render(graphics);
